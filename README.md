540c2c8d41ebdcdb25ad34371f4ee639eeaff5e2
Samyukta <>
2019-04-12 11:51:38 -0700

    Adding readme

---
+++ README.md
@@ -0,0 +1,6 @@
+## Restaurent Menu App
+
+Objective
+=========
+Restaurent menu app is created to show the menu and on selected to display the items.
+
--- app/components/menuList.js
+++ app/components/menuList.js
@@ -1,27 +1,17 @@
 import React from 'react';
 
-class MenuList extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-		}
-
-	}
-
-
-	render () {
-
-		return (
-			<div className="menuList">
-				{this.props.items.map(item => {
-					return <li key={item.id} 
-						onClick={() => this.props.onSelect(item)}>
-						{item.name} - ({item.short_name})
-					</li>
-				})}
-			</div>
-		)
-	}
+const MenuList = (props) => {	
+	return (
+		<div className="menuList">
+			{props.items.map(item => {
+				return <li key={item.id} 
+					onClick={() => props.onSelect(item)}>
+					{item.name} - ({item.short_name})
+				</li>
+			})}
+		</div>
+	)
+	
 }
 
 export default MenuList;
